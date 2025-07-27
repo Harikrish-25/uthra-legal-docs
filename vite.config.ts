bbd8cd8b21b0410e4474b681e14ef41bca321fa8
Harikrish-25 <kharikrishnan2525@gmail.com>
2025-07-27 11:29:47 +0530

    error solved

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // This is the correct setting for Netlify
+  base: "/", // This is the correct setting for Netlify/deployment
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
